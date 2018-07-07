cc9ada8f1ca6d67545111ca4376513564a941ba8
michael.deselincourt <michael.deselincourt@REPLGROUP.COM>
2018-07-07 20:23:35 +0100

    Randomisation uses the input as a seed

---
+++ procworld.js
@@ -0,0 +1,14 @@
+			$(document).ready(function(){
+				
+			});
+			
+			function begin() {
+				
+				$("#output").html("hello world");
+				
+				Math.seedrandom($("#input").val());
+				console.log(Math.random());          // Always 0.9282578795792454
+				
+			};
+			
+
